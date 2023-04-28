cad9cad37dcc35b4baca9788c119a3d0e95653cb
Marcos Bueno <marcos.filho@facens.br>
2023-04-28 17:41:37 -0300

    loading component

--- App.tsx
+++ App.tsx
@@ -6,10 +6,14 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
 
+import { Loading } from '@components/Loading';
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>{fontsLoaded ? <View /> : <View />}</NativeBaseProvider>
+    <NativeBaseProvider>
+      <Loading />
+    </NativeBaseProvider>
   );
 }
