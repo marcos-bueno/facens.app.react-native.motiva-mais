8f82ab56bc05a7a8b59fb7aa363d3ffd5a66d3ae
Marcos Bueno <marcos.filho@facens.br>
2023-04-28 16:34:44 -0300

    font roboto

--- App.tsx
+++ App.tsx
@@ -1,9 +1,16 @@
 import { Text, View } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {fontsLoaded ? <Text>Hello World</Text> : <View />}
     </View>
   );
 }
